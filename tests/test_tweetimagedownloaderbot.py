e8f7d6d28c6ec3182b0f19efa85252c1284f6d08
chika <fumifumibot2323@gmail.com>
2021-09-26 14:01:39 +0900

    テスト用スクリプトを追加

--- tests/test_tweetimagedownloaderbot.py
+++ tests/test_tweetimagedownloaderbot.py
@@ -1,5 +1,22 @@
-from tweetimagedownloaderbot import __version__
+import os
+
+import urllib.request
+from helper.twittermanager import TwitterManager
+
+from bot import __version__
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == "0.1.0"
+
+
+if __name__ == "__main__":
+    # 検証したいURLを入れる
+    request_url = ""
+
+    manager = TwitterManager()
+    imageurls = manager.get_image_urls(request_url)
+
+    for url in imageurls:
+        filename = os.path.basename(url)
+        urllib.request.urlretrieve(url, filename)
